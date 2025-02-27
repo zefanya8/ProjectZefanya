5a918e107e9e6658f9c25c6157d40a0265312383
zefanya8 <zefanyamaranatha8@gmail.com>
2025-02-27 16:22:14 +0700

    Add files via upload

---
+++ sheets.js
@@ -0,0 +1,79 @@
+const sheetID = '1XnBK3wbRUnKTPXHKJ4hpFQUSBZAZLiaJfOJYF-AIHJU';
+const base = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?`;
+const sheetName = 'sheet1';
+const query = encodeURIComponent('Select *');
+const url = `${base}&sheet=${sheetName}&tq=${query}`;
+const data = [];
+
+document.addEventListener('DOMContentLoaded', init);
+
+const output = document.querySelector('.output');
+ 
+function init() {
+    console.log('ready');
+    fetch(url)
+        .then(res => res.text())
+        .then(rep => {
+            data.length = 0;  
+            output.innerHTML = ""; 
+            const jsData = JSON.parse(rep.substring(47).slice(0, -2));
+
+            const colz = [];
+                            
+            jsData.table.cols.forEach((heading) => {
+                if(heading.label){
+                    colz.push(heading.label.toLowerCase());
+                }
+                // console.log(colz)
+            });
+            
+            
+            jsData.table.rows.forEach((main) =>{
+           
+                const row = {};
+               
+                colz.forEach((ele, ind)=>{
+                  
+                    
+                    row[ele] = (main.c[ind] != null) ? main.c[ind].v : '';
+                    // console.log(row)
+                })
+                console.log(row)
+                
+                data.push(row);
+                
+               
+            })
+            maker(data);
+        });
+    
+}
+
+function maker(json){
+
+    json.forEach((el)=>{
+        const div = document.createElement('div');
+        div.classList.add('card', 'mt-3','mx-auto');
+        output.append(div);
+        const keys = Object.keys(el);
+
+        keys.forEach(
+            (key) => {
+                const value = el[key];
+                console.log(value)
+                const ele = document.createElement('p');
+                if (key === 'nama'){
+                    ele.classList.add('card-header', 'tebal');
+                    ele.textContent = value;
+                    div.append(ele);
+                }
+                if (key === 'ucapan'){
+                    const komenAnak = document.createElement('p');
+                    komenAnak.textContent = `\u201C${value}\u201D`;
+                    komenAnak.classList.add('miring');
+                    div.append(komenAnak);
+                }
+            }
+        )
+    })
+}
